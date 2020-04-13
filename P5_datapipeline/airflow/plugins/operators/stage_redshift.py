eea8c5964ebbbdfb606045d020fdc0ff2600fa38
root <mazenorabi@gmail.com>
2020-04-13 13:12:46 +0000

    p5

---
+++ P5_datapipeline/airflow/plugins/operators/stage_redshift.py
@@ -0,0 +1,90 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+    template_fields = ("s3_key",)
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        REGION '{}'
+        TIMEFORMAT as 'epochmillisecs'
+        TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL
+        {} 'auto' 
+        {}
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 region="",
+                 file_format="JSON",
+                 delimiter=",",
+                 ignore_headers=1,
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.region= region
+        self.file_format = file_format
+        self.aws_credentials_id = aws_credentials_id
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.execution_date = kwargs.get('execution_date')
+
+    def execute(self, context):
+        """
+            Copy data from S3 buckets to redshift cluster into staging tables.
+                - redshift_conn_id: redshift cluster connection
+                - aws_credentials_id: AWS connection
+                - table: redshift cluster table name
+                - s3_bucket: S3 bucket name holding source data
+                - s3_key: S3 key files of source data
+                - file_format: source file format - options JSON, CSV
+                - delimiter: CSV file delimiter
+                - ignore_headers: to ignore CSV file header (1,0)
+        """
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info("Copying data from S3 to Redshift")
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        csv_additional=""
+        if self.execution_date:
+            # Backfill a specific date
+            year = self.execution_date.strftime("%Y")
+            month = self.execution_date.strftime("%m")
+            day = self.execution_date.strftime("%d")
+            s3_path = '/'.join([s3_path, str(year), str(month), str(day)])
+        if self.file_format == 'CSV':
+            csv_additional = f" DELIMETER '{self.delimiter}' IGNOREHEADER {self.ignore_headers} "
+        formatted_sql = S3ToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.region,
+            self.file_format,
+            csv_additional
+        )
+        redshift.run(formatted_sql)
+
+
+
+
+
